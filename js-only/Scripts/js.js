21c321096e019b3d8ec8fb38bb6faac03d2734fe
tatianadzn <tatiana22d@gmail.com>
2019-04-01 22:46:26 +0300

    generates image

--- js-only/Scripts/js.js
+++ js-only/Scripts/js.js
@@ -1,13 +1,18 @@
-function createCanvas() {
+let canvas = document.createElement('canvas');
+const body =  document.getElementsByTagName('body')[0];
+body.appendChild(canvas);
+canvas.width = 400;
+canvas.height = 400;
+let ctx = canvas.getContext('2d');
 
-    let canvas = document.createElement('canvas');
-    const body =  document.getElementsByTagName('body')[0];
-    body.appendChild(canvas);
-    canvas.width = 256;
-    canvas.height = 256;
-    let ctx = canvas.getContext('2d');
-    ctx.fillRect(0,0, canvas.width, canvas.height);
-    return canvas;
-}
 
-canv = createCanvas();
+pic = new Image();
+pic.src = 'https://source.unsplash.com/collection/1127163/400x400';
+
+pic.onload = function () {
+    ctx.drawImage(pic, 0, 0, canvas.width, canvas.height);
+    ctx.font = "48px serif";
+    ctx.fillStyle = "white";
+    ctx.fillText("quote", 100, canvas.height/2);
+};
+
